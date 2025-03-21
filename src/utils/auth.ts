e4015dad3200bbf1a9a44653a83c923cd7e1123b
M <1925871213@qq.com>
2025-03-21 19:51:08 +0800

    feat: 添加Prisma集成以支持数据库操作

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/utils/prisma";
 
 import GitHub from "next-auth/providers/github";
 import Gitee from "@/utils/gitee";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub,
     Gitee({
